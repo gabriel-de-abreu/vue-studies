9b0d17733505b7da60122e29f2b23a2928a7e93f
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-07-23 18:56:53 -0300

    vue cli

---
+++ classes/vue-cli/src/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    <app-server-status v-for="s in 5"></app-server-status>
+</div>
+</template>
+
+
+<script>
+    import ServerStatus from './ServerStatus.vue'
+    export default {
+        components:{
+            'app-server-status': ServerStatus
+        }
+    }
+</script>
---
+++ classes/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>Server Status: {{status}} <p>
+        <hr>
+        <button @click="changeStatus">Change Status</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data : ()=>{
+            return {
+                status: 'Normal'
+            }
+        },
+        methods: {
+            changeStatus(){
+                if(this.status==='Critical'){
+                    this.status='Normal';
+                }else{
+                    this.status='Critical'
+                }
+            }
+        }
+    };
+</script>
