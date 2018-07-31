ced514f4f22f9432be929aabb91c761419f2dff8
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-07-31 17:36:49 -0300

    starting task 7

--- classes/adv-components/src/App.vue
+++ classes/adv-components/src/App.vue
@@ -2,17 +2,40 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>        
+                <hr>
+                <p>{{selectedComponent}}</p>        
+                <component :is = "selectedComponent">
+                    <p slot = "content">Default content</p>
+                </component>
+               <!-- <app-quote>
+                   <h2 slot = "title">{{quoteTitle}}</h2>
+                   <p slot = "content">A wonderful quote!</p>
+                </app-quote> -->
             </div>
         </div>
     </div>
 </template>
 
 <script>
-
-    export default {
-        
-    }
+import appQuote from "./components/Quote.vue";
+import appAuthor from "./components/Author.vue";
+import appNew from "./components/New.vue";
+export default {
+  data: () => {
+    return {
+      quoteTitle: "The Quote",
+      selectedComponent: 'appQuote'
+    };
+  },
+  components: {
+    appQuote,
+    appAuthor,
+    appNew
+  }
+};
 </script>
 
 <style>
