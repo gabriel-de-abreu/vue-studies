3776f03a9bee59e185b7942d30876464666a271a
Gabriel de Abreu <gabrieldeabreu.ti@gmail.com>
2018-07-31 20:44:20 -0300

    2nd project

---
+++ projects/project-2/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount ='quotes.length' :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"  @quoteDeleted= "deleteQuote"></app-quote-grid>
+        <div class = "row">
+            <div class = "col-sm-12 text-center">
+                <div class = "alert alert-info">Info: Click on a quote to delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import appQuoteGrid from './components/QuoteGrid.vue'
+    import appNewQuote from './components/NewQuote.vue'
+    import appHeader from './components/Header.vue'
+    
+    export default {
+        data: ()=>{
+            return {
+                quotes:['Just some content!'],
+                maxQuotes:10,
+            }
+        },
+        methods:{
+            newQuote(quote){
+                if(this.quotes.length >= this.maxQuotes){
+                    return alert('Please delete quotes first!')
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index){
+                this.quotes.splice(index,1);
+            }
+        },
+        components:{
+            appQuoteGrid,
+            appNewQuote,
+            appHeader
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ projects/project-2/src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class = "row">
+        <app-quote v-for = "(quote,index) in quotes" :key="quote"
+        @click.native="deleteQuote(index)">{{quote}}</app-quote>
+    </div>
+</template>
+
+<script>
+    import appQuote from './Quote.vue'
+    export default {
+        props:['quotes'],
+        components:{
+            appQuote
+        },
+        methods:{
+            deleteQuote(index){
+                this.$emit('quoteDeleted',index);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    
+</style>
+
